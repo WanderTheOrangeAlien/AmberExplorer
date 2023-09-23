15243aef04c20c39ad64e4433f0394db688039e3
WanderTheOrangeAlien <01miguelurena@gmail.com>
2023-09-23 13:44:51 -0400

    Seleccion de Tiempo + Arnes

---
+++ Assets/Scripts/Game Management/Timer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public static float time;
+    public static float targetTime;
+
+    public static bool isRunning = false;
+
+    public float DEBUG_time;
+    public float DEBUG_targetTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DEBUG_time = time;
+        DEBUG_targetTime = targetTime;
+
+        if (isRunning)
+        {
+            time += Time.deltaTime;
+        }
+        
+        if(time >= targetTime && isRunning)
+        {
+            Debug.LogWarning("TIME RAN OUT!");
+            StopTimer();
+        }
+
+    }
+
+    public static void StartTimer()
+    {
+        Debug.Log("Timer Started. Target Time=" + targetTime);
+        time = 0;
+        isRunning = true;
+    }
+
+    public static void StopTimer()
+    {
+        isRunning = false;
+    }
+
+}
